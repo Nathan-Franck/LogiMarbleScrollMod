2a95ab9a730feead466b924a915c2ef32e8fca7d
Nathan Franck <nathanfranck1@gmail.com>
2022-07-29 00:53:48 -0600

    This method has no lag but doesn't work :(

--- LogiMarbleScrollMod.cpp
+++ LogiMarbleScrollMod.cpp
@@ -20,47 +20,37 @@ int main()
     MSG msg;
     gHook = SetWindowsHookEx(WH_MOUSE_LL, &LowLevelMouseProc, GetModuleHandle(0), 0);
     std::cout << "Running!" << std::endl;
-    while (GetMessage(&msg, NULL, 0, 0))
+    while (1)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                return 0;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        Sleep(10);
     }
 
     return 0;
 }
 
-int sendAmount = 0;
 LRESULT CALLBACK LowLevelMouseProc(int code, WPARAM wParam, LPARAM lParam)
 {
     MSLLHOOKSTRUCT* hookStruct = (MSLLHOOKSTRUCT*)lParam;
-
-    // Ignore injected events
-    if (code < 0 || (hookStruct->flags & (LLMHF_INJECTED | LLKHF_INJECTED | LLMHF_LOWER_IL_INJECTED | LLKHF_LOWER_IL_INJECTED))) {
-        return CallNextHookEx(gHook, code, wParam, lParam);
-    }
 
     if (code == HC_ACTION)
     {
         switch (wParam)
         {
         case WM_MOUSEMOVE: {
-            int dy = hookStruct->pt.y - scrollingCursorPos.y;
+            short dy = hookStruct->pt.y - scrollingCursorPos.y;
 
             if (scrollingActive)
             {
-                if (sendAmount > 80)
-                    break;
-                INPUT input;
-                input.type = INPUT_MOUSE;
-                input.mi.dwFlags = MOUSEEVENTF_WHEEL;
-                input.mi.time = NULL; //Windows will do the timestamp
-                input.mi.mouseData = (DWORD)dy; //A positive value indicates that the wheel was rotated forward, away from the user; a negative value indicates that the wheel was rotated backward, toward the user. One wheel click is defined as WHEEL_DELTA, which is 120.
-                input.mi.dx = scrollingCursorPos.x;
-                input.mi.dy = scrollingCursorPos.y;
-                input.mi.dwExtraInfo = GetMessageExtraInfo();
-                sendAmount ++;
-
-                auto result = SendInput(1, &input, sizeof(INPUT));
+                hookStruct->mouseData = dy << 16;
+                std::cout << (short)(hookStruct->mouseData >> 16) << std::endl;
+                CallNextHookEx(gHook, code, WM_MOUSEWHEEL, (LPARAM)hookStruct);
                 return 1;
             }
             
@@ -69,8 +59,11 @@ LRESULT CALLBACK LowLevelMouseProc(int code, WPARAM wParam, LPARAM lParam)
         case WM_RBUTTONDOWN: {
             scrollingActive = true;
             scrollingCursorPos = hookStruct->pt;
-            sendAmount = 0;
             return 1;
+        }
+        case WM_MOUSEWHEEL: {
+            std::cout << (short)(hookStruct->mouseData >> 16) << std::endl;
+            break;
         }
         case WM_RBUTTONUP: {
             scrollingActive = false;
